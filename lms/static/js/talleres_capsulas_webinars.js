6fdb7f69675b8f8b12eae06c533af5d497673cce
Vicente Daie Pinilla <vdaiep@gmail.com>
2023-08-05 17:57:21 -0400

    talleres, capsulas y webinars, + fixes

--- lms/static/js/talleres_capsulas_webinars.js
+++ lms/static/js/talleres_capsulas_webinars.js
@@ -21,6 +21,8 @@ function showTalleresWebinarsCapsulas(display, displayId) {
                 setUrlParameter('displayId', items["defaultItem"]["id"]);
             }
             fillTalleres(items);
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            fillTalleres({"active": null, "default": null, "summarizedItems": []})
         });
     } else {
         $.getJSON('https://static.redfid.cl/capsulas/capsulas.json', function(data){
@@ -29,18 +31,14 @@ function showTalleresWebinarsCapsulas(display, displayId) {
                 setUrlParameter('displayId', items["defaultItem"]["id"]);
             }
             fillCapsulas(items);
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            fillTalleres({"active": null, "default": null, "summarizedItems": []})
         });
     }
-    $("#back-to-dashboard").on("click", function() {
-        var currentUrl = new URL(window.location.href);
-        currentUrl.search = "";
-        window.location.href = currentUrl.toString();
-    });
-    
 }
 
 function hideTalleresWebinarsCapsulas() {
-    $("#talleres-webinars-capsulas-main").hide();
+    $("#twc-main").hide();
     $("#talleres-image").on("click", function(){
         var currentUrl = new URL(window.location.href);
         currentUrl.searchParams.set('display', "1");
@@ -56,15 +54,158 @@ function hideTalleresWebinarsCapsulas() {
 }
 
 function fillTalleres(items){
-    console.log(items);
-    // ver caso vacio
-    // sino rellenar
+    $("#twc-main").html(`
+    <a class="back-to-landing-button" href="/dashboard">
+        <i class="fa fa-arrow-left" aria-hidden="true"></i>
+        Volver a Aprendizaje Profesional
+    </a>
+    <h1 class="landing-title">Talleres y webinars</h1>
+    <p class="landing-description">Aquí podrás volver a revisar los talleres y webinars impartidos hacia la comunidad de formadores de RedFID.</p>
+    <hr>
+    <div class="twc-container">
+        <div class="twc-content">
+            <div class="twc-content-error-container" style="display: none;">
+                <p>El taller o webinar seleccionado no existe.</p>
+            </div>
+            <h1 class="twc-content-title"></h1>
+            <div class="twc-content-video-container"></div>
+            <div class="twc-content-subtitle-container"></div>
+            <p class="twc-content-description"></p>
+            <p class="twc-content-exposes"></p>
+        </div>
+        <hr class="twc-mobile-separator">
+        <div class="twc-summary">
+        </div>
+    </div>
+    <div class="twc-error-container" style="display: none;">
+        <p>No hay talleres ni webinars disponibles.</p>
+    </div>
+    `);
+    if (items.active == null && items.summarizedItems.length === 0){
+        $(".twc-container").hide();
+        $(".twc-error-container").show();
+    } else {
+        if (items.active == null){
+            $(".twc-content-error-container").show();
+        } else {
+            $(".twc-content-title").text(items.active.title);
+            var videoEmbed = `<iframe src="${convertToEmbedUrl(items.active.video_url)}" frameborder="0" allowfullscreen></iframe>`;
+            $(".twc-content-video-container").append(videoEmbed);
+            $(".twc-content-subtitle-container").append(`
+                <div class="twc-content-tag" style="background-color: ${items.active.kind === "taller" ? "#A5D6D9" : "#eb947e6e"};">${items.active.kind}</div>
+                <div class="twc-content-date">${items.active.date}</div>
+            `);
+            $(".twc-content-description").text(items.active.description);
+            $(".twc-content-exposes").text(items.active.exposes);
+        }
+        if (items.summarizedItems.length !== 0) {
+            var first = true;
+            items.summarizedItems.sort((a, b) => b.priority - a.priority);
+            for(let item of items.summarizedItems) {
+                if (!first) {
+                    $(".twc-summary").append("<hr>")
+                }
+                first = false;
+                $(".twc-summary").append(`
+                    <h1 class="twc-summary-title">${item.title}</h1>
+                    <div class="twc-summary-image-container">
+                        <img src="${convertToThumbnailUrl(item.video_url)}" alt="${item.title}">
+                    </div>
+                    <div class="twc-summary-subtitle-container">
+                        <div class="twc-summary-tag" style="background-color: ${item.kind === "taller" ? "#A5D6D9" : "#eb947e6e"};">${item.kind}</div>
+                        <div class="twc-summary-date">${item.date}</div>
+                    </div>
+                    <p class="twc-summary-description">${item.description}</p>
+                    <p class="twc-summary-exposes">${item.exposes}</p>
+                    <div class="twc-summary-button-container">
+                        <a class="twc-summary-button" href="/dashboard?display=1&displayId=${item.id}" target="_self">Ver ${item.kind}</a>
+                    </div>
+                `);
+            }
+        } else {
+            $(".twc-summary").append(`
+                <p class="twc-summary-empty-message">¡Próximamente más talleres y webinars!</p>
+            `);
+        }
+        $(".twc-container").show();
+        $(".twc-error-container").hide();
+    }
 }
 
-function fillCapsulas(items){ 
-    console.log(items);
-    // ver caso vacio
-    // sino rellanar
+function fillCapsulas(items){
+    $("#twc-main").html(`
+    <a class="back-to-landing-button" href="/dashboard">
+        <i class="fa fa-arrow-left" aria-hidden="true"></i>
+        Volver a Aprendizaje Profesional
+    </a>
+    <h1 class="landing-title">Cápsulas</h1>
+    <p class="landing-description">Aquí podrás acceder a cápsulas con contenido pedagógico que puede resultar útil para tu formación docente.</p>
+    <hr>
+    <div class="twc-container">
+        <div class="twc-content">
+            <div class="twc-content-error-container" style="display: none;">
+                <p>La cápsula seleccionada no existe.</p>
+            </div>
+            <h1 class="twc-content-title"></h1>
+            <div class="twc-content-video-container"></div>
+            <div class="twc-content-subtitle-container"></div>
+            <p class="twc-content-description"></p>
+        </div>
+        <hr class="twc-mobile-separator">
+        <div class="twc-summary">
+        </div>
+    </div>
+    <div class="twc-error-container" style="display: none;">
+        <p>No hay cápsulas disponibles.</p>
+    </div>
+    `);
+    if (items.active == null && items.summarizedItems.length === 0){
+        $(".twc-container").hide();
+        $(".twc-error-container").show();
+    } else {
+        if (items.active == null){
+            $(".twc-content-error-container").show();
+        } else {
+            $(".twc-content-title").text(items.active.title);
+            var videoEmbed = `<iframe src="${convertToEmbedUrl(items.active.video_url)}" frameborder="0" allowfullscreen></iframe>`;
+            $(".twc-content-video-container").append(videoEmbed);
+            $(".twc-content-subtitle-container").append(`
+                <div class="twc-content-tag" style="background-color: #A5D6D9">${items.active.tag}</div>
+                <div class="twc-content-date">${items.active.date}</div>
+            `);
+            $(".twc-content-description").text(items.active.description);
+        }
+        if (items.summarizedItems.length !== 0) {
+            var first = true;
+            items.summarizedItems.sort((a, b) => b.priority - a.priority);
+            for(let item of items.summarizedItems) {
+                if (!first) {
+                    $(".twc-summary").append("<hr>")
+                }
+                first = false;
+                $(".twc-summary").append(`
+                    <h1 class="twc-summary-title">${item.title}</h1>
+                    <div class="twc-summary-image-container">
+                        <img src="${convertToThumbnailUrl(item.video_url)}" alt="${item.title}">
+                    </div>
+                    <div class="twc-summary-subtitle-container">
+                        <div class="twc-summary-tag" style="background-color: #A5D6D9">${item.tag}</div>
+                        <div class="twc-summary-date">${item.date}</div>
+                    </div>
+                    <p class="twc-summary-description">${item.description}</p>
+                    <div class="twc-summary-button-container">
+                        <a class="twc-summary-button" href="/dashboard?display=2&displayId=${item.id}" target="_self">Ver cápsula</a>
+                    </div>
+                `);
+            }
+        } else {
+            $(".twc-summary").append(`
+                <p class="twc-summary-empty-message">¡Próximamente más cápsulas!</p>
+            `);
+        }
+        $(".twc-container").show();
+        $(".twc-error-container").hide();
+    }
 }
 
 function getUrlParameter(sParam) {
@@ -102,7 +243,7 @@ function getAndClassifyItems(data, displayId){
     defaultItem = null;
     summarizedItems = [];
     data.forEach((item, index) => {
-        if (item.id.toString() === displayId){
+        if (item.id === displayId){
             activeItem = item;
         }
         if (defaultItem == null){
@@ -113,11 +254,29 @@ function getAndClassifyItems(data, displayId){
         }
         summarizedItems.push(item);
     });
-    if (activeItem == null){
+    if (activeItem == null && displayId === 0){
         activeItem = defaultItem;
     }
-    summarizedItems = summarizedItems.filter(function(item) {
-        return item.id !== activeItem.id;
-    });
+    if (activeItem != null){
+        summarizedItems = summarizedItems.filter(function(item) {
+            return item.id !== activeItem.id;
+        });
+    }
     return {"active": activeItem, "summarizedItems": summarizedItems, "defaultItem": defaultItem}
+}
+
+function convertToEmbedUrl(youtubeUrl) {
+    const match = youtubeUrl.match(/v=([\w-]+)/);
+    if (match && match[1]) {
+        return 'https://www.youtube.com/embed/' + match[1];
+    }
+    return youtubeUrl;
+}
+
+function convertToThumbnailUrl(youtubeUrl) {
+    const match = youtubeUrl.match(/v=([\w-]+)/);
+    if (match && match[1]) {
+        return 'https://img.youtube.com/vi/' + match[1] + '/mqdefault.jpg';
+    }
+    return null;
 }
