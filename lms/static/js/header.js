77cfbc4a7e795aafe748bc8bf0cc6866d356b47a
Vicente Daie Pinilla <vdaiep@gmail.com>
2024-05-31 12:41:38 -0400

    home, courses and dashboard course listing ready

--- lms/static/js/header.js
+++ lms/static/js/header.js
@@ -1,8 +1,10 @@
-function toggleMenu() {
+function toggleLinksMenu() {
     var navContainer = document.querySelector('.header-navigation-container-mobile');
+    var userContainer = document.querySelector('.header-user-container-mobile');
     var content = document.querySelector('#content');
 
     if (navContainer.style.display === 'none' || navContainer.style.display === '') {
+        userContainer.style.display = 'none';
         navContainer.style.display = 'block';
         content.style.marginTop = '220px';
     } else {
@@ -11,10 +13,27 @@ function toggleMenu() {
     }
 }
 
+function toogleUserMenu() {
+    var userContainer = document.querySelector('.header-user-container-mobile');
+    var navContainer = document.querySelector('.header-navigation-container-mobile');
+    var content = document.querySelector('#content');
+
+    if (userContainer.style.display === 'none' || userContainer.style.display === '') {
+        navContainer.style.display = 'none';
+        userContainer.style.display = 'block';
+        content.style.marginTop = '220px';
+    } else {
+        userContainer.style.display = 'none';
+        content.style.marginTop = '0';
+    }
+}
+
 window.onclick = function(event) {
     var navContainer = document.querySelector('.header-navigation-container-mobile');
+    var userContainer = document.querySelector('.header-user-container-mobile');
     if (!event.target.matches('.header-trigger')) {
         navContainer.style.display = 'none';
+        userContainer.style.display = 'none';
         document.querySelector('main').style.marginTop = '0';
     }
 }
@@ -25,5 +44,6 @@ window.onresize = function(event) {
     if (screenWidth > 940) {
         content.style.marginTop = '0';
         document.querySelector('.header-navigation-container-mobile').style.display = 'none';
+        document.querySelector('.header-user-container-mobile').style.display = 'none';
     }
 };
