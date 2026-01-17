30323c85949fe95922231799ca293715170c65df
Vicente Daie Pinilla <vdaiep@gmail.com>
2026-01-17 00:35:22 -0300

    feat: oauth2 login

--- cms/static/js/header.js
+++ cms/static/js/header.js
@@ -22,35 +22,65 @@ function toggleUserMenu() {
     }
 }
 
-window.onclick = function(event) {
-    var navContainer = document.querySelector('.header-navigation-container-mobile');
-    var userContainer = document.querySelector('.header-user-container-mobile');
-    if (navContainer.style.display !== "none") {
-        if (!event.target.matches('.header-navigation-container-mobile')) {
-            navContainer.style.display = 'none';
+// Use addEventListener to avoid overriding other click handlers
+(function() {
+    function handleClick(event) {
+        var navContainer = document.querySelector('.header-navigation-container-mobile');
+        var userContainer = document.querySelector('.header-user-container-mobile');
+        if (navContainer.style.display !== "none") {
+            if (!event.target.matches('.header-navigation-container-mobile')) {
+                navContainer.style.display = 'none';
+            }
+        } else {
+            if (event.target.matches('.header-trigger')) {
+                toggleLinksMenu();
+            }
         }
-    } else {
-        if (event.target.matches('.header-trigger')) {
-            toggleLinksMenu();
+        if (userContainer.style.display !== "none") {
+            if (!event.target.matches('.header-user-container-mobile')) {
+                userContainer.style.display = 'none';
+            }
+        } else {
+            if (event.target.matches('.toggle-user-menu')) {
+                toggleUserMenu();
+            }
         }
     }
-    if (userContainer.style.display !== "none") {
-        if (!event.target.matches('.header-user-container-mobile')) {
-            userContainer.style.display = 'none';
-        }
+    
+    if (window.addEventListener) {
+        window.addEventListener('click', handleClick, false);
+    } else if (window.attachEvent) {
+        window.attachEvent('onclick', handleClick);
     } else {
-        if (event.target.matches('.toggle-user-menu')) {
-            toggleUserMenu();
-        }
+        var oldOnClick = window.onclick;
+        window.onclick = function(event) {
+            if (oldOnClick) oldOnClick(event);
+            handleClick(event);
+        };
     }
-}
+})();
 
-window.onresize = function(event) {
-    var screenWidth = window.innerWidth;
-    var navContainer = document.querySelector('.header-navigation-container-mobile');
-    if (screenWidth > 940) {
-        navContainer.style.display = 'none';
-        document.querySelector('.header-navigation-container-mobile').style.display = 'none';
-        document.querySelector('.header-user-container-mobile').style.display = 'none';
+// Use addEventListener to avoid overriding other resize handlers
+(function() {
+    function handleResize(event) {
+        var screenWidth = window.innerWidth;
+        var navContainer = document.querySelector('.header-navigation-container-mobile');
+        if (screenWidth > 940) {
+            navContainer.style.display = 'none';
+            document.querySelector('.header-navigation-container-mobile').style.display = 'none';
+            document.querySelector('.header-user-container-mobile').style.display = 'none';
+        }
     }
-};
+    
+    if (window.addEventListener) {
+        window.addEventListener('resize', handleResize, false);
+    } else if (window.attachEvent) {
+        window.attachEvent('onresize', handleResize);
+    } else {
+        var oldOnResize = window.onresize;
+        window.onresize = function(event) {
+            if (oldOnResize) oldOnResize(event);
+            handleResize(event);
+        };
+    }
+})();
